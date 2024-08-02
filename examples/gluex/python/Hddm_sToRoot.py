d7c16a3afedaa67f48d88076b14e19f2091a4a0f
dleahy27 <dillon.leahy@proton.me>
2024-08-02 09:21:52 +0100

    Updated Hddm_s - Now generalised, PID function gets from root, commented out progress bar until I can fix getting total event number

--- examples/gluex/python/Hddm_sToRoot.py
+++ examples/gluex/python/Hddm_sToRoot.py
@@ -1,4 +1,4 @@
-# %% Note uses Python2 as HDDM dependancy
+# Note uses Python2 as HDDM dependancy
 import hddm_s
 import os
 import sys
@@ -7,18 +7,21 @@ import ROOT
 from array import array
 import math
 from time import time
-# %%
 
-# Define a c++ struct
 ROOT.gInterpreter.Declare("""
 struct event {
     std::vector<double> P{0,0,0}; 
     std::vector<double> Theta{0,0,0}; 
     std::vector<double> Phi{0,0,0};
 };
+
+struct beam {
+    double P = 0;
+    double Theta = 0;
+    double Phi = 0; 
+};
 """)
 
-# Progress bar to command line
 def ProgressBar(j, initial_wall_time, N ):
     i = j-1
     a = int(20*i/N)
@@ -37,95 +40,62 @@ def ProgressBar(j, initial_wall_time, N ):
     sys.stdout.flush()
 
     return
-           
-
-
-def StringToPdg(particle):
-    # Convert particle names to their PDG value
-    # Only working with pi+- proton and gamma at the moment
-    if particle == "Gamma":
-        return 22
-    elif particle == "Pi+":
-        return 211
-    elif particle == "Pi-":
-        return -211
-    elif particle == "Proton":
-        return 2212
-    else:
-        return 
 
 def CartesianToSpherical(momentum_xyz):
     # Takes in momentum class from Hddm library
-    # Converts cartesian momentum to momentum norm and spherical coordinates
     p = math.sqrt((momentum_xyz.px)**2 + (momentum_xyz.py)**2 + (momentum_xyz.pz)**2)
     phi = math.atan2(momentum_xyz.py,momentum_xyz.px)
     theta = math.acos(momentum_xyz.pz/p)
 
     return p,theta,phi
 
-def HddmToRoot(input_name, output_name):
+def GetPid(_particle):
+    return pdg.Instance().GetParticle(_particle).PdgCode() 
+
+def HddmToRoot(input_name, output_name, particles):
     # Initialise output file and TTree
     myFile = ROOT.TFile.Open(output_name+".root", "RECREATE")
     tree = ROOT.TTree("tree", "Generated_Reaction_Data")
 
-    # Initialise TTree branches
-    # As accepted is a boolean, need to initialise using C++
-    # Right now setting all to true
-    # Will be read in by rec code
-#     accepted_code = """
-# Bool_t accepted_p=kTRUE;
-# tree->Branch(Form("accepted_p"),&accepted_p);
-# Bool_t accepted_pip=kTRUE;
-# tree->Branch(Form("accepted_pip"),&accepted_pip);
-# Bool_t accepted_pim=kTRUE;
-# tree->Branch(Form("accepted_pim"),&accepted_pim);
-# """
-#     # Run above C++ code
-#     ROOT.gInterpreter.ProcessLine(accepted_code)
-
-    # beam = ...
+    beam = ROOT.beam()
     truth = ROOT.event()
 
     tree.Branch("truth",truth)
-    #tree.Branch("beam", , )
+    tree.Branch("beam", beam)
 
     # Start ProgressBar counters
-    i = 1
-    initial_wall_time = time()
+    # For now comment out until I can get total number of events from HDDM
+    #i = 1
+    #initial_wall_time = time()
     
     # Load file and skip first event (always nothing)
     try:
         f = iter(hddm_s.istream(input_name))
-        ProgressBar(int(i), initial_wall_time, 10**6)
-        i += 1
+        # ProgressBar(int(i), initial_wall_time, 10**6)
+        # i += 1
     except:
         # Only print as we still want the program to run
         print "The file, ", input_name, " cannot be read."
 
     # Loop over all the events
     for rec in f:
-        ProgressBar(int(i), initial_wall_time, 10**6)
+        # ProgressBar(int(i), initial_wall_time, 10**6)
         for reaction in rec.getReactions():
-            # If beam data is wanted fill out below
-            # for beam in reaction.getBeams():
+            for Beam in reaction.getBeams():
+                for momentum in Beam.getMomenta():
+                    beam.P, beam.Theta, beam.Phi = CartesianToSpherical(momentum)
             for product in reaction.getVertices()[0].getProducts():
                 pdg  = product.pdgtype
-                # Check pdg of each event (only want proton and pi+-)
-                # Find a way to generalise this
-                # Probably read in particles to keep it consistent, can then loop over i.e
-                # for particle in particles, if pdg = pdg then i+= 1 and set array
-                if pdg == 2212:
-                    for momentum in product.getMomenta():
-                        truth.P[0], truth.Theta[0], truth.Phi[0] = CartesianToSpherical(momentum)
-                elif pdg == 211:
-                    for momentum in product.getMomenta():
-                        truth.P[1], truth.Theta[1], truth.Phi[1] = CartesianToSpherical(momentum)
-                elif pdg == -211:
-                    for momentum in product.getMomenta():
-                        truth.P[2], truth.Theta[2], truth.Phi[2] = CartesianToSpherical(momentum)
-                else:
-                    continue
-
+                
+                i=0
+                for particle in particles:
+                    if particle == "gamma":
+                        continue # Gammas cant be reconstructed
+                    elif GetPid(particle) == pdg:
+                        truth.P[i], truth.Theta[i], truth.Phi[i] = CartesianToSpherical(momentum)
+                        i += 1
+                        continue       
+                continue
         i += 1
 
         # Fill tree and write it to file
@@ -134,21 +104,13 @@ def HddmToRoot(input_name, output_name):
 
 if __name__ == '__main__':
     # Read in arguments from bash script command line
-    input_data = sys.argv[1] 
+    input_data = sys.argv[1]
+    particles = sys.argv[2].split(',') 
     output_name = os.path.join("data", "gluex_reaction")
-    # maybe particles to generalise?
 
     # Call Hddm Function 
-    #HddmToRoot(input_data, output_name)
-    df = ROOT.RDataFrame("tree", output_name+".root")
-    df = df.Define("pP","truth.P[0]")
-    df.Display("pP",10).Print()
-
-# %%
-# Sort progress bar => find way to extract total events from hddm
-# Something about putting in branch data, can ask tuesday
-# Find a way to generalise better. Idea on line 113
-# %%
+    HddmToRoot(input_data, output_name, particles)
+    
 
 
 
