d98d22f0da8bbf3140bd37721fc16852836ed900
dleahy27 <dillon.leahy@proton.me>
2024-08-04 10:59:13 +0100

    adding the new files

---
+++ examples/gluex/Training.py
@@ -0,0 +1,48 @@
+# Python script to handle Macparticles simulation
+# Import and define constants
+import os 
+import sys
+import time
+
+py = "./python/"
+macparticles = "root -q ../../macros/Load.C" 
+
+def Train(particles,resparticles=[""], id=False):
+    #Run Configure file
+    if id:
+        os.system(macparticles+" 'Configure_Rad.C'")
+    else:
+        os.system(macparticles+" 'Configure_Hddm.C'")
+        
+           
+        
+    
+    # Train for each particle
+    for i in range(len(particles)):
+        if id:
+            # os.system(f"""{macparticles} 'RunAcceptanceTraining.C( "{particles[i]}", "{resparticles[i]}" )'""")
+            # os.system(f"""{macparticles} 'RunReweightTraining.C( "{particles[i]}", "{resparticles[i]}" )'""")
+            os.system(f"""{macparticles} 'RunResolutionTraining.C( "{particles[i]}", "{resparticles[i]}" )'""")
+        else:
+            # os.system(f"""{macparticles} 'RunAcceptanceTraining.C( "{particles[i]}" )'""")
+            # os.system(f"""{macparticles} 'RunReweightTraining.C( "{particles[i]}" )'""")
+            os.system(f"""{macparticles} 'RunResolutionTraining.C( "{particles[i]}" )'""")
+            
+            
+        i += 1
+
+if __name__ == '__main__':
+    # Execute program
+    start_time = time.time()
+    
+    if len(sys.argv) > 2:
+        Train(sys.argv[1].split(','), sys.argv[2].split(','), True)
+    else:
+        Train(sys.argv[1].split(','))
+         
+    # Print out script run time
+    remaining = (time.time() - start_time)         
+    mins, secs = divmod(remaining, 60)
+    hours, mins = divmod(mins, 60)
+    time_str = str(f"""Total training runtime - {int(hours)}:{int(mins)}:{round(secs,2)}""")
+    print(time_str)
