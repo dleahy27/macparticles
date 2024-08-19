05fd07b741a22f7820b6c4ed7831b66674ded43e
dleahy27 <dillon.leahy@proton.me>
2024-08-19 13:17:19 +0100

    Updated Configure scripts

---
+++ examples/gluex/Configure_etaphi.C
@@ -0,0 +1,123 @@
+{
+
+
+  auto info = TrainingInfo{"k+"};
+
+  info.treeName = "simtree";
+  info.reconFile = "./data/kp_training.root";
+  
+  //what fraction of events in each file do you want to use?
+  info.recFrac = 1;
+
+  //truth name, generated name, reconstructed name, title, range
+  info.variables ={{"truP","truP","recP","#pi+ momentum",0,10},
+		   {"truTheta","truTheta","recTheta","#pi+ #theta",0,TMath::Pi()},
+		   {"truPhi","truPhi","recPhi","#pi+ #phi",-TMath::Pi(),TMath::Pi()}};
+
+  //variable to flag if particle was reconstructed in this event
+  info.acceptVar = "accepted";
+
+  //in case need further fltering of reconstructed events (e.g. truth matching,..)
+  info.recFilter = "";
+
+
+  info.WriteTo("training.root");
+  /*********************************************************************************************************/
+  info = TrainingInfo{"k-"};
+
+
+  info.treeName = "simtree";
+  info.reconFile = "./data/km_training.root";
+  
+  //what fraction of events in each file do you want to use?
+  info.recFrac = 1;
+
+  //truth name, generated name, reconstructed name, title, range
+  info.variables = 	{{"truP","truP","recP","#pi- momentum",0,10},
+			 {"truTheta","truTheta","recTheta","#pi- #theta",0,TMath::Pi()},
+			 {"truPhi","truPhi","recPhi","#pi- #phi",-TMath::Pi(),TMath::Pi()}};
+
+  //variable to flag if particle was reconstructed in this event
+  info.acceptVar = "accepted";
+
+  //in case need further fltering of reconstructed events (e.g. truth matching,..)
+  info.recFilter = "";
+
+ 
+
+  info.WriteTo("training.root");
+
+ /*********************************************************************************************************/
+  info = TrainingInfo{"proton"};
+
+
+  info.treeName = "simtree";
+  info.reconFile = "./data/proton_training.root";
+  
+  //what fraction of events in each file do you want to use?
+  info.recFrac = 1;
+
+  //truth name, generated name, reconstructed name, title, range
+  info.variables = 	{{"truP","truP","recP","#proton momentum",0,10},
+			 {"truTheta","truTheta","recTheta","#proton #theta",0,TMath::Pi()},
+			 {"truPhi","truPhi","recPhi","#proton #phi",-TMath::Pi(),TMath::Pi()}};
+
+  //variable to flag if particle was reconstructed in this event
+  info.acceptVar = "accepted";
+
+  //in case need further fltering of reconstructed events (e.g. truth matching,..)
+  info.recFilter = "";
+
+
+  info.WriteTo("training.root");
+
+  /*********************************************************************************************************/
+  info = TrainingInfo{"gamma1"};
+
+
+  info.treeName = "simtree";
+  info.reconFile = "./data/gamma_training.root";
+  
+  //what fraction of events in each file do you want to use?
+  info.recFrac = 1;
+
+  //truth name, generated name, reconstructed name, title, range
+  info.variables = 	{{"truP","truP","recP","#proton momentum",0,10},
+			 {"truTheta","truTheta","recTheta","#proton #theta",0,TMath::Pi()},
+			 {"truPhi","truPhi","recPhi","#proton #phi",-TMath::Pi(),TMath::Pi()}};
+
+  //variable to flag if particle was reconstructed in this event
+  info.acceptVar = "accepted";
+
+  //in case need further fltering of reconstructed events (e.g. truth matching,..)
+  info.recFilter = "";
+
+
+  info.WriteTo("training.root");
+
+  /*********************************************************************************************************/
+  info = TrainingInfo{"gamma2"};
+
+
+  info.treeName = "simtree";
+  info.reconFile = "./data/gamma_training.root";
+  
+  //what fraction of events in each file do you want to use?
+  info.recFrac = 1;
+
+  //truth name, generated name, reconstructed name, title, range
+  info.variables = 	{{"truP","truP","recP","#proton momentum",0,10},
+			 {"truTheta","truTheta","recTheta","#proton #theta",0,TMath::Pi()},
+			 {"truPhi","truPhi","recPhi","#proton #phi",-TMath::Pi(),TMath::Pi()}};
+
+  //variable to flag if particle was reconstructed in this event
+  info.acceptVar = "accepted";
+
+  //in case need further fltering of reconstructed events (e.g. truth matching,..)
+  info.recFilter = "";
+
+
+  info.WriteTo("training.root");
+
+
+}
